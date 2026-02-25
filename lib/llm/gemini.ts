c0fb27f421721d24ee7fa5fd66f3d9cc4e7c852a
aais1 <aaisali228@gmail.com>
2026-02-25 06:20:10 +0500

    openai

--- lib/llm/gemini.ts
+++ lib/llm/gemini.ts
@@ -2,9 +2,9 @@ import OpenAI from "openai";
 
 export const OPENAI_CHAT_MODEL = "gpt-3.5-turbo";
 
-const OPENAI_API_KEY ="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";
+const THIS ="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";
 
-const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: THIS });
 
 export async function generateText(opts: {
   prompt: string;
